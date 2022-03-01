6246dab215bce125ba20bb05ed4d49834a994785
itshea <73254818+itshea@users.noreply.github.com>
2022-03-01 00:33:13 -0600

    player movement implemented

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 let PhaserConfig = {
     type: Phaser.Auto,
-    parent: "game",
-    width: 1280,
-    height: 720,
+    //parent: "game",
+    width: 800,
+    height: 600,
+	physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         init: initScene,
         preload: preloadScene,
         create: createScene,
         update: updateScene
-        
     }
 };
 
@@ -16,15 +22,23 @@ let game = new Phaser.Game(PhaserConfig);
 
 var snowman;
 var background;
+var platforms;
+var cursors;
 
 function initScene() {}
 
 function preloadScene() {
     this.load.spritesheet("snowman", "snowman.png", {frameWidth: 300, frameHeight: 300});
 	this.load.image("background1", "assets/pixilart-drawing.png");
+	//this.load.image("ground", "assets/ground.png");
 }
 
 function createScene() {
+	//background
+	background = this.add.image(400, 300, "background1");
+	background.setScale(2);
+	
+	//player
     this.anims.create({
         key: "stand",
         frameRate: 2,
@@ -32,12 +46,41 @@ function createScene() {
         repeat: 10
     });
     
-    snowman = this.add.sprite(640,360, "snowman");
+    snowman = this.physics.add.sprite(240,500, "snowman");
     snowman.play("stand");
 	
-	background = this.add.image(300, 300, "background1");
-	background.setScale(2);
+	//player = this.physics.add.sprite(100, 450, 'dude');
 
+	snowman.setBounce(0.2);
+	snowman.setCollideWorldBounds(true);
+	
+	cursors = this.input.keyboard.createCursorKeys();
+	
+	//platform
+	//platforms = this.physics.add.staticGroup();
+	//platforms.create(400, 300, 'ground').setScale(2).refreshBody();
+	
 }
 
-function updateScene() {}
+function updateScene() {
+	if (cursors.left.isDown)
+	{
+		snowman.setVelocityX(-160);
+
+	}
+	else if (cursors.right.isDown)
+	{
+		snowman.setVelocityX(160);
+
+	}
+	else
+	{
+		snowman.setVelocityX(0);
+
+	}
+
+	if (cursors.up.isDown && snowman.body.touching.down)
+	{
+		snowman.setVelocityY(-330);
+	}
+}
